86f842150db2b87b7f552000a675fa35fd9ccb79
soyeon <ksy1118s@naver.com>
2025-01-27 23:48:58 +0900

    feat: ChatMessage 추가

---
+++ src/main/kotlin/com/example/nbe233team9/domain/chat/entity/ChatMessage.kt
@@ -0,0 +1,43 @@
+package com.example.nbe233team9.domain.chat.entity
+
+import com.example.nbe233team9.domain.user.model.User
+import jakarta.persistence.*
+import java.awt.TrayIcon.MessageType
+import java.time.LocalDateTime
+
+@Entity
+@Table(name = "chat_messages")
+class ChatMessage (
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    var id: Long? = null,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "sender_id", nullable = false)
+    var sender: User,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "receiver_id")
+    var receiver: User? = null,
+
+    @Column(name = "content", nullable = false, length = 1000)
+    var content: String,
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "type", nullable = false)
+    var type: MessageType,
+
+    @Column(name = "sent_at", nullable = false)
+    var sentAt: LocalDateTime = LocalDateTime.now()
+){
+    @PrePersist
+    fun onCreate(){
+        this.sentAt = LocalDateTime.now()
+    }
+
+    enum class MessageType{
+        TALK,
+        SYSTEM
+    }
+}

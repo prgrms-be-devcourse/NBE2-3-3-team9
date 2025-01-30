710df712c9b52a344237e5d8a26ccc8e1b5a8426
양한동 <yhd0121@gs.anyang.ac.kr>
2025-01-30 13:53:05 +0900

    feat: auth부분 토큰재발급, 로그아웃기능 추가

--- src/main/kotlin/com/example/nbe233team9/domain/auth/controller/AuthController.kt
+++ src/main/kotlin/com/example/nbe233team9/domain/auth/controller/AuthController.kt
@@ -2,6 +2,7 @@ package com.example.nbe233team9.domain.auth.controller
 
 import com.example.nbe233team9.common.response.ApiResponse
 import com.example.nbe233team9.domain.auth.dto.TokenResponseDTO
+import com.example.nbe233team9.domain.auth.security.CustomUserDetails
 import com.example.nbe233team9.domain.auth.security.JwtTokenProvider
 import com.example.nbe233team9.domain.auth.service.AuthService
 import com.example.nbe233team9.domain.auth.service.KakaoService
@@ -9,9 +10,12 @@ import com.example.nbe233team9.domain.user.dto.LoginAdminDTO
 import com.example.nbe233team9.domain.user.dto.UserResponseDTO
 import io.swagger.v3.oas.annotations.Operation
 import jakarta.servlet.http.Cookie
+import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
 import jakarta.validation.Valid
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.security.access.prepost.PreAuthorize
+import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.web.bind.annotation.GetMapping
 
 import org.springframework.web.bind.annotation.PostMapping
@@ -44,6 +48,22 @@ class AuthController(
         val admin = authService.login(loginAdminDTO, response)
         return ApiResponse.ok(admin);
     }
+
+    @Operation(summary = "로그아웃")
+    @GetMapping("/logout")
+    @PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
+    fun logout(@AuthenticationPrincipal userDetails: CustomUserDetails, response: HttpServletResponse) : ApiResponse<String> {
+        val logout = authService.logout(userDetails.getUserId(), response);
+        return ApiResponse.ok(logout);
+    }
+
+    @PostMapping("/new-token")
+    @Operation(summary = "토큰 재발급", description = "access토큰 재발급")
+    fun reCreate(request: HttpServletRequest) :ApiResponse<TokenResponseDTO> {
+        val reCreate = authService.reCreateToken(request)
+        return ApiResponse.ok(reCreate);
+    }
+
 
     @Operation(summary = "카카오 로그인")
     @GetMapping("/kakao")

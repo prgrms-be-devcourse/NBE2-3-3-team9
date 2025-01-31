4851fa6bfd4adcf921c57b4bbbcdccff32321ab4
Yu-Hyeon-Su <ydasfg@gmail.com>
2025-01-31 17:05:47 +0900

    feat: Pet 로그인 기능 추가

--- src/main/kotlin/com/example/nbe233team9/domain/pet/controller/PetController.kt
+++ src/main/kotlin/com/example/nbe233team9/domain/pet/controller/PetController.kt
@@ -1,9 +1,12 @@
 package com.example.nbe233team9.domain.pet.controller
 
 import com.example.nbe233team9.common.response.ApiResponse
+import com.example.nbe233team9.domain.auth.security.CustomUserDetails
 import com.example.nbe233team9.domain.pet.dto.PetDTO
 import com.example.nbe233team9.domain.pet.service.PetService
 import org.springframework.http.MediaType
+import org.springframework.security.access.prepost.PreAuthorize
+import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.web.bind.annotation.*
 import org.springframework.web.multipart.MultipartFile
 
@@ -13,28 +16,35 @@ class PetController(
     private val petService: PetService
 ) {
     @GetMapping("/pets")
-    fun findPets(): ApiResponse<List<PetDTO>> {
-        val pets = petService.findPets(4L);
+    @PreAuthorize("hasRole('USER')")
+    fun findPets(@AuthenticationPrincipal userDetails: CustomUserDetails): ApiResponse<List<PetDTO>> {
+        val pets = petService.findPets(userDetails.getUserId());
         return ApiResponse.ok(pets);
     }
 
     @GetMapping("/pets/{petId}")
-    fun getPetDetails(@PathVariable petId: Long): ApiResponse<PetDTO> {
-        val pet = petService.getPetDetails(4L, petId)
+    @PreAuthorize("hasRole('USER')")
+    fun getPetDetails(@PathVariable petId: Long,
+                      @AuthenticationPrincipal userDetails: CustomUserDetails): ApiResponse<PetDTO> {
+        val pet = petService.getPetDetails(userDetails.getUserId(), petId)
         return ApiResponse.ok(pet);
     }
 
     @PostMapping(value = ["/pet"], consumes = [MediaType.MULTIPART_FORM_DATA_VALUE])
+    @PreAuthorize("hasRole('USER')")
     fun addPets(@RequestPart(value = "dto" ) request: PetDTO.AddPetDTO,
-                @RequestPart(value = "file", required = false ) file: MultipartFile?): ApiResponse<PetDTO> {
-        val pet = petService.addPet(request, file, 4L)
+                @RequestPart(value = "file", required = false ) file: MultipartFile?,
+                @AuthenticationPrincipal userDetails: CustomUserDetails): ApiResponse<PetDTO> {
+        val pet = petService.addPet(request, file, userDetails.getUserId())
         return ApiResponse.ok(pet);
     }
 
     @PutMapping(value = ["/pet/{petId}"], consumes = [MediaType.MULTIPART_FORM_DATA_VALUE])
+    @PreAuthorize("hasRole('USER')")
     fun updatePets(@RequestPart(value = "dto" ) request: PetDTO.UpdatePetDTO,
-                   @RequestPart(value = "file", required = false ) file: MultipartFile?): ApiResponse<PetDTO> {
-        val pet = petService.updatePet(request, file, 4L)
+                   @RequestPart(value = "file", required = false ) file: MultipartFile?,
+                   @AuthenticationPrincipal userDetails: CustomUserDetails): ApiResponse<PetDTO> {
+        val pet = petService.updatePet(request, file, userDetails.getUserId())
         return ApiResponse.ok(pet);
     }
 

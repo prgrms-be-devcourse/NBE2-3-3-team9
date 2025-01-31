ad803d37e32010fccafaa0908fb5d48cfe21ca77
Yu-Hyeon-Su <ydasfg@gmail.com>
2025-01-31 16:44:39 +0900

    feat: 스케줄에 로그인 기능 추가

--- src/main/kotlin/com/example/nbe233team9/domain/schedule/controller/PeriodicScheduleController.kt
+++ src/main/kotlin/com/example/nbe233team9/domain/schedule/controller/PeriodicScheduleController.kt
@@ -1,8 +1,11 @@
 package com.example.nbe233team9.domain.schedule.controller
 
 import com.example.nbe233team9.common.response.ApiResponse
+import com.example.nbe233team9.domain.auth.security.CustomUserDetails
 import com.example.nbe233team9.domain.schedule.dto.PeriodicScheduleDTO
 import com.example.nbe233team9.domain.schedule.service.PeriodicScheduleService
+import org.springframework.security.access.prepost.PreAuthorize
+import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.web.bind.annotation.*
 
 @RestController
@@ -11,22 +14,27 @@ class PeriodicScheduleController(
     private val periodicScheduleService: PeriodicScheduleService
 ) {
     @GetMapping("/periodicSchedules")
-    fun findPeriodicSchedules(): ApiResponse<List<PeriodicScheduleDTO>> {
-        val periodicScheduleDTOs: List<PeriodicScheduleDTO> = periodicScheduleService.findPeriodicSchedules(4L)
+    @PreAuthorize("hasRole('USER')")
+    fun findPeriodicSchedules(@AuthenticationPrincipal userDetails: CustomUserDetails): ApiResponse<List<PeriodicScheduleDTO>> {
+        val periodicScheduleDTOs: List<PeriodicScheduleDTO> = periodicScheduleService.findPeriodicSchedules(userDetails.getUserId())
         return ApiResponse.ok(periodicScheduleDTOs)
     }
 
     @PostMapping("/periodicSchedule")
+    @PreAuthorize("hasRole('USER')")
     fun addPeriodicSchedule(
-        @RequestBody request: PeriodicScheduleDTO.AddPeriodicScheduleDTO): ApiResponse<PeriodicScheduleDTO> {
-        val periodicScheduleDTO: PeriodicScheduleDTO = periodicScheduleService.addPeriodicSchedule(request,4L)
+        @RequestBody request: PeriodicScheduleDTO.AddPeriodicScheduleDTO,
+        @AuthenticationPrincipal userDetails: CustomUserDetails): ApiResponse<PeriodicScheduleDTO> {
+        val periodicScheduleDTO: PeriodicScheduleDTO = periodicScheduleService.addPeriodicSchedule(request,userDetails.getUserId())
         return ApiResponse.ok(periodicScheduleDTO)
     }
 
     @PutMapping("/periodicSchedule/{scheduleId}")
+    @PreAuthorize("hasRole('USER')")
     fun updatePeriodicSchedule(
-        @RequestBody request: PeriodicScheduleDTO.UpdatePeriodicScheduleDTO): ApiResponse<PeriodicScheduleDTO> {
-        val periodicScheduleDTO = periodicScheduleService.updatePeriodicSchedule(request, 4L)
+        @RequestBody request: PeriodicScheduleDTO.UpdatePeriodicScheduleDTO,
+        @AuthenticationPrincipal userDetails: CustomUserDetails): ApiResponse<PeriodicScheduleDTO> {
+        val periodicScheduleDTO = periodicScheduleService.updatePeriodicSchedule(request, userDetails.getUserId())
         return ApiResponse.ok(periodicScheduleDTO)
     }
 

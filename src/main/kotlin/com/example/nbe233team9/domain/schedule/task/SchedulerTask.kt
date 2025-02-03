9aa6bf4327f48d26b565379487c32e6fbee97bd4
unknown <ydasfg@gmail.com>
2025-02-03 10:47:30 +0900

    feat: 스케줄 조회 페이징 처리

--- src/main/kotlin/com/example/nbe233team9/domain/schedule/task/SchedulerTask.kt
+++ src/main/kotlin/com/example/nbe233team9/domain/schedule/task/SchedulerTask.kt
@@ -3,8 +3,10 @@ package com.example.nbe233team9.domain.schedule.task
 import com.example.nbe233team9.domain.schedule.repository.SingleScheduleRepository
 import com.example.nbe233team9.domain.schedule.service.MessageService
 import com.example.nbe233team9.domain.schedule.service.RedisService
+import org.springframework.data.domain.PageRequest
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
+import java.awt.print.Pageable
 import java.time.LocalDateTime
 
 
@@ -15,32 +17,39 @@ class SchedulerTask(
     private val redisService: RedisService
 ) {
 
-    @Scheduled(fixedRate = 10000 ) // 5분마다 실행
+    @Scheduled(fixedRate = 10000) // 5분마다 실행
     fun requestMessage() {
-        val schedules = singleScheduleRepository.findSchedulesWithinNextTenMinutes(
-            LocalDateTime.now(),
-            LocalDateTime.now().plusMinutes(10)
-        )
+        var page = 0
+        val pageSize = 50
+        do {
+            // 일정 페이징 조회
+            val schedulePage = singleScheduleRepository.findSchedulesWithinNextTenMinutes(
+                LocalDateTime.now(),
+                LocalDateTime.now().plusMinutes(10),
+                PageRequest.of(page, pageSize)
+            )
 
-        // 전송할 일정 목록 필터링 및 처리
-        val scheduleIds = schedules
-            .filter { schedule ->
-                val redisKey = "user.${schedule.user.id}.access_token"
-                redisService.getValues(redisKey) != null
-            }
-            .onEach { schedule ->
-                val redisKey = "user.${schedule.user.id}.access_token"
-                val accessToken = redisService.getValues(redisKey)
-                if (accessToken != null) {
-                    messageService.requestMessage(accessToken, schedule)
+            val scheduleIds = schedulePage.content
+                .filter { schedule ->
+                    val redisKey = "user.${schedule.user.id}.access_token"
+                    redisService.getValues(redisKey) != null
                 }
-            }
-            .map { it.id }
+                .onEach { schedule ->
+                    val redisKey = "user.${schedule.user.id}.access_token"
+                    val accessToken = redisService.getValues(redisKey)
+                    if (accessToken != null) {
+                        messageService.requestMessage(accessToken, schedule)
+                    }
+                }
+                .map { it.id }
 
-        // Batch Update 실행
-        if (scheduleIds.isNotEmpty()) {
-            singleScheduleRepository.updateNotificationTime(scheduleIds, LocalDateTime.now())
-        }
+            // Batch Update 실행
+            if (scheduleIds.isNotEmpty()) {
+                singleScheduleRepository.updateNotificationTime(scheduleIds, LocalDateTime.now())
+            }
+
+            page++
+        } while (schedulePage.hasNext())  // 다음 페이지가 있으면 반복
     }
 }
 
